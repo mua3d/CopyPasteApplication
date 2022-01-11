2860dec8b360a1683b697c59ea10491d474ec2f8
moadh <moad-3000@hotmail.com>
2022-01-11 21:53:15 +0300

    navigation manager wip

--- app/src/main/java/com/example/copypasteapplication/MainActivity.kt
+++ app/src/main/java/com/example/copypasteapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.copypasteapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.copypasteapplication.fragments.HomeFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = HomeFragment()
+        this.supportFragmentManager.beginTransaction()
+            .replace(R.id.activity_main_container, fragment)
+            .addToBackStack(this::class.java.name)
+            .commit()
     }
 }
